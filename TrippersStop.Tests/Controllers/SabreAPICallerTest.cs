a1f90482850bf7812625749382dd6a79234dc6f6
madhurim <madhuri.mittal@gmail.com>
2015-02-02 17:47:05 -0500

    Added some code for BargainFinder and example test

--- TrippersStop.Tests/Controllers/SabreAPICallerTest.cs
+++ TrippersStop.Tests/Controllers/SabreAPICallerTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TrippersStop.TraveLayer;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using TraveLayer.CustomTypes.Sabre;
 
 
 namespace TrippersStop.Tests.Controllers
@@ -74,5 +75,119 @@ namespace TrippersStop.Tests.Controllers
             Assert.IsNotNull(result);
 
         }
+        [TestMethod]
+        public void SerializationTest()
+        {
+            BargainFinder bf = new BargainFinder();
+            bf.OTA_AirLowFareSearchRQ = new OTAAirLowFareSearchRQ();
+
+            bf.OTA_AirLowFareSearchRQ.OriginDestinationInformation = new System.Collections.Generic.List<OriginDestinationInformation>();
+
+            OriginDestinationInformation odinfoOrig = new OriginDestinationInformation();
+            odinfoOrig.DestinationLocation = new DestinationLocation();
+            odinfoOrig.DestinationLocation.LocationCode = "EWR";
+            odinfoOrig.OriginLocation = new OriginLocation();
+            odinfoOrig.OriginLocation.LocationCode = "DFW";
+            odinfoOrig.RPH = 1;
+            odinfoOrig.DepartureDateTime = "2014-11-11T00:00:00";
+            bf.OTA_AirLowFareSearchRQ.OriginDestinationInformation.Add(odinfoOrig);
+
+            OriginDestinationInformation odinfoDest = new OriginDestinationInformation();
+            odinfoDest.DestinationLocation = new DestinationLocation();
+            odinfoDest.DestinationLocation.LocationCode = "DFW";
+            odinfoDest.OriginLocation = new OriginLocation();
+            odinfoDest.OriginLocation.LocationCode = "EWR";
+            odinfoDest.RPH = 1;
+            odinfoDest.DepartureDateTime = "2014-11-20T00:00:00";
+            bf.OTA_AirLowFareSearchRQ.OriginDestinationInformation.Add(odinfoOrig);
+
+            bf.OTA_AirLowFareSearchRQ.POS = new POS();
+            bf.OTA_AirLowFareSearchRQ.POS.Source = new System.Collections.Generic.List<Source>();
+            bf.OTA_AirLowFareSearchRQ.POS.Source.Add(new Source()
+            {
+                RequestorID = new RequestorID() { CompanyName = new CompanyName() { Code = "TN" }, ID = "REQ.ID", Type = "0.AAA.X" }
+
+            });
+
+            bf.OTA_AirLowFareSearchRQ.TPA_Extensions = new TPAExtensions();
+            bf.OTA_AirLowFareSearchRQ.TPA_Extensions.IntelliSellTransaction = new IntelliSellTransaction();
+            bf.OTA_AirLowFareSearchRQ.TPA_Extensions.IntelliSellTransaction.RequestType = new RequestType()
+            {
+                Name = "50ITINS"
+
+            };
+
+
+            ServiceStack.Text.JsonSerializer<BargainFinder> seri = new ServiceStack.Text.JsonSerializer<BargainFinder>();
+            String bfJson = seri.SerializeToString(bf);
+
+        }
     }
+
+    /*{
+    "OTA_AirLowFareSearchRQ": {
+        "OriginDestinationInformation": [
+            {
+                "DepartureDateTime": "2014-11-11T00:00:00",
+                "DestinationLocation": {
+                    "LocationCode": "JFK"
+                },
+                "OriginLocation": {
+                    "LocationCode": "LAS"
+                },
+                "RPH": 1
+            },
+            {
+                "DepartureDateTime": "2014-11-12T00:00:00",
+                "DestinationLocation": {
+                    "LocationCode": "LAS"
+                },
+                "OriginLocation": {
+                    "LocationCode": "JFK"
+                },
+                "RPH": 2
+            }
+        ],
+        "POS": {
+            "Source": [
+                {
+                    "RequestorID": {
+                        "CompanyName": {
+                            "Code": "TN"
+                        },
+                        "ID": "REQ.ID",
+                        "Type": "0.AAA.X"
+                    }
+                }
+            ]
+        },
+        "TPA_Extensions": {
+            "IntelliSellTransaction": {
+                "RequestType": {
+                    "Name": "50ITINS"
+                }
+            }
+        },
+        "TravelPreferences": {
+            "TPA_Extensions": {
+                "NumTrips": {
+                    "Number": 1
+                }
+            }
+        },
+        "TravelerInfoSummary": {
+            "AirTravelerAvail": [
+                {
+                    "PassengerTypeQuantity": [
+                        {
+                            "Code": "ADT",
+                            "Quantity": 1
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+}
+*/
 }
