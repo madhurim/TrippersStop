2d1304185a7393bb78b5f4e8ed43e7cfbd0d287a
Manoj <er.manojkhandelwal@hotmail.com>
2015-03-09 15:18:13 +0530

    Created method to get url from reflection

--- TrippersStop/Areas/Sabre/Controllers/DestinationsController.cs
+++ TrippersStop/Areas/Sabre/Controllers/DestinationsController.cs
@@ -23,7 +23,6 @@ namespace TrippersStop.Areas.Sabre.Controllers
             string url = GetURL(destinationsRequest);
             return GetResponse(url);
         }
-
         private string GetURL(Destinations destinationsRequest)
         {
             StringBuilder url = new StringBuilder();
@@ -50,39 +49,6 @@ namespace TrippersStop.Areas.Sabre.Controllers
             }
             return url.ToString();
         }
-        // GET api/DestinationFinder
-        //public HttpResponseMessage Get(string origin, string departuredate, string returndate)
-        //{
-        //    string url = string.Format("v1/shop/flights/fares?origin={0}&departuredate={1}&returndate={2}", origin, departuredate, returndate);
-        //    return GetResponse(url);
-        //}
-        //// GET api/DestinationFinder
-        //public HttpResponseMessage Get(string origin, string lengthofstay)
-        //{
-        //    string url = string.Format("v1/shop/flights/fares?origin={0}&lengthofstay={1}", origin, lengthofstay);
-        //    return GetResponse(url);
-        //}
-        //public HttpResponseMessage Get(string origin, string earliestdeparturedate, string latestdeparturedate, string lengthofstay)
-        //{
-        //    string url = string.Format("v1/shop/flights/fares?origin={0}&earliestdeparturedate={1}&latestdeparturedate={2}&lengthofstay={3}", origin, earliestdeparturedate, latestdeparturedate, lengthofstay);
-        //    return GetResponse(url);
-        //}
-        //public HttpResponseMessage Get(string origin, string earliestdeparturedate, string latestdeparturedate, string lengthofstay, string theme)
-        //{
-        //    //string url = string.Format("v1/shop/flights/fares?origin={0}&earliestdeparturedate={1}&latestdeparturedate={2}&lengthofstay={3}&theme={4}", destinationFinderRequest.Origin, destinationFinderRequest.Earliestdeparturedate, destinationFinderRequest.Latestdeparturedate, destinationFinderRequest.Lengthofstay, destinationFinderRequest.Theme);
-        //    string url = string.Format("v1/shop/flights/fares?origin={0}&earliestdeparturedate={1}&latestdeparturedate={2}&lengthofstay={3}&theme={4}", origin, earliestdeparturedate, latestdeparturedate, lengthofstay, theme);
-        //    return GetResponse(url);
-        //}
-        //public HttpResponseMessage GetDestinationByMaxFare(string origin, string departuredate, string returndate, string maxfare)
-        //{
-        //    string url = string.Format("v1/shop/flights/fares?origin={0}&departuredate={1}&returndate={2}&maxfare={3}", origin, departuredate, returndate, maxfare);
-        //    return GetResponse(url);
-        //}
-        //public HttpResponseMessage GetTopDestination(string origin, string departuredate, string returndate, string topdestinations)
-        //{
-        //    string url = string.Format("v1/shop/flights/fares?origin={0}&departuredate={1}&returndate={2}&topdestinations={3}", origin, departuredate, returndate, topdestinations);
-        //    return GetResponse(url);
-        //}
         private HttpResponseMessage GetResponse(string url)
         {
             string result = APIHelper.GetDataFromSabre(url);
