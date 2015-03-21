e3ac1c5e77687f103795b457ea1ff5ceeddaba9b
saurabhg0784@gmail.com <saurabh@123>
2015-03-21 16:34:39 +0530

    TESTCASES DONE FOR RESPONSE

---
+++ TrippersStop.Tests/SabreAPITests/TestResponse/CountriesLookup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TraveLayer.APIServices;
+using TraveLayer.CustomTypes;
+using TraveLayer.CustomTypes.Sabre;
+using TrippersStop.TraveLayer;
+
+namespace TrippersStop.Tests.SabreAPITests
+{
+    [TestClass]
+    public class CountriesLookup
+    {
+        [TestMethod]
+
+        public void GetTest()
+        {
+            SabreAPICaller apiWrapper = new SabreAPICaller();
+
+            apiWrapper.Accept = "application/json";
+            apiWrapper.ContentType = "application/x-www-form-urlencoded";
+
+            string token = apiWrapper.GetToken().Result;
+
+            apiWrapper.Authorization = "bearer";
+
+            string result = apiWrapper.Get("v1/lists/supported/countries?pointofsalecountry=DE").Result;
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ TrippersStop.Tests/SabreAPITests/TestResponse/InstaFlightsSearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TraveLayer.APIServices;
+using TraveLayer.CustomTypes;
+using TraveLayer.CustomTypes.Sabre;
+using TrippersStop.TraveLayer;
+
+
+namespace TrippersStop.Tests.SabreAPITests
+{
+     [TestClass]
+    public class InstaFlightsSearch
+    {
+        [TestMethod]
+
+        public void GetTest()
+        {
+            SabreAPICaller apiWrapper = new SabreAPICaller();
+
+            apiWrapper.Accept = "application/json";
+            apiWrapper.ContentType = "application/x-www-form-urlencoded";
+
+            string token = apiWrapper.GetToken().Result;
+
+            apiWrapper.Authorization = "bearer";
+
+            string result = apiWrapper.Get("v1/shop/flights?origin=DFW&destination=CLE&departuredate=2015-07-03&returndate=2015-07-12").Result;
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
