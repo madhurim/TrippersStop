d35d2d77e5c2975769e7abd49388646920517242
Manoj <er.manojkhandelwal@hotmail.com>
2015-03-28 22:38:22 +0530

    Clean up folders and newtonsoft

---
+++ TraveLayer/CustomTypes/Sabre/ViewModels/TopDestination.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraveLayer.CustomTypes.Sabre.ViewModels
+{
+    public class TopDestination
+    {
+        public string OriginLocation { get; set; }
+        public List<Destination> Destinations { get; set; }
+    }
+}
