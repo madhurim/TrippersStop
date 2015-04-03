b9020d14e9643f1f2e2f4ed9f3b163f351e654f2
Manoj <er.manojkhandelwal@hotmail.com>
2015-04-03 11:29:16 +0530

    Weather - Redis save

--- TrippersStop/Areas/Weather/Controllers/HistoryController.cs
+++ TrippersStop/Areas/Weather/Controllers/HistoryController.cs
@@ -14,6 +14,7 @@ namespace Trippism.Areas.Weather.Controllers
 {
     public class HistoryController : ApiController
     {
+        const string TrippismKey = "Trippism.Weather."; 
         IAsyncWeatherAPICaller _apiCaller;
         ICacheService _cacheService;
         /// <summary>
@@ -32,18 +33,23 @@ namespace Trippism.Areas.Weather.Controllers
         [HttpGet]
         public HttpResponseMessage Get([FromUri]HistoryInput weatherInfo)
         {
-
+            string cacheKey = TrippismKey+string.Join(".", weatherInfo.City , weatherInfo.State , weatherInfo.DepartDate.ToShortDateString() , weatherInfo.ReturnDate.ToShortDateString());
+            var tripWeather = _cacheService.GetByKey<TripWeather>(cacheKey);
+            if (tripWeather != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, tripWeather);
+            }
             //http://localhost:14606/api/weather/history?State=CA&City=San_Francisco&DepartDate=2015-07-06&ReturnDate=2015-07-09
             // http://api.wunderground.com/api/Your_Key/planner_MMDDMMDD/q/CA/San_Francisco.json
             string fromDate = weatherInfo.DepartDate.ToString("MMdd");
             string toDate = weatherInfo.ReturnDate.ToString("MMdd");
             string url = string.Format("planner_{0}{1}/q/{2}/{3}.json", fromDate, toDate, weatherInfo.State, weatherInfo.City);
-            return GetResponse(url);
+            return GetResponse(url, cacheKey);
         }
         /// <summary>
         /// Get response from api based on url.
         /// </summary>
-        private HttpResponseMessage GetResponse(string url)
+        private HttpResponseMessage GetResponse(string url, string cacheKey)
         {
             _apiCaller.Accept = "application/json";
             _apiCaller.ContentType = "application/json";
@@ -64,6 +70,8 @@ namespace Trippism.Areas.Weather.Controllers
                Mapper.CreateMap<TempLow, TempLowAvg>()
                   .ForMember(h => h.Avg, m => m.MapFrom(s => s.avg));
                 TripWeather tripWeather = Mapper.Map<Trip, TripWeather>(trip);
+                _cacheService.Save<TripWeather>(cacheKey, tripWeather);
+                 
                 HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, tripWeather);
                 return response;
             }
