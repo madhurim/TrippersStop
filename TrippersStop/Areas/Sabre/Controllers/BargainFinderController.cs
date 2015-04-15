c31aba4a765533318d94b73377e7d92baa2012b1
Manoj <er.manojkhandelwal@hotmail.com>
2015-04-15 22:23:54 +0530

    POS configuration changes

--- TrippersStop/Areas/Sabre/Controllers/BargainFinderController.cs
+++ TrippersStop/Areas/Sabre/Controllers/BargainFinderController.cs
@@ -14,7 +14,7 @@ using System.Runtime.Serialization;
 using TraveLayer.CustomTypes.Sabre.ViewModels;
 using System.Xml;
 using System.Xml.Linq;
-
+using System.Web.Hosting;
 
 namespace TrippersStop.Areas.Sabre.Controllers
 {
@@ -30,37 +30,48 @@ namespace TrippersStop.Areas.Sabre.Controllers
         // POST api/bargainfinder
         public HttpResponseMessage Post(BargainFinder bargainFinder)
         {
-            //BargainFinder bFinder = new BargainFinder();
-            ////bFinder.
-            //POS pos = new POS();
-            //XElement xelement = XElement.Load(System.Web.Hosting.HostingEnvironment.MapPath("/POS/PointOfSales.xml"));
-            //var element = from nm in xelement.Elements("POS")
-            //           where (string)nm.Element("IsActive") == "true" && (string)nm.Element("RequestType") == "BargainFinder"
-            //           select nm;
-            ////XmlDocument xmlDocument = new XmlDocument();
-            ////xmlDocument.Load(@"\POS\PointOfSales.xml");
-            ////XmlNodeList nodeList = xmlDocument.DocumentElement.SelectNodes("/PointOfSales");
-            ////var node =nodeList
-            ////string proID = "", proName = "", price = "";
-            ////foreach (XmlNode node in nodeList)
-            ////{
-            ////    proID = node.SelectSingleNode("Product_id").InnerText;
-            ////    proName = node.SelectSingleNode("Product_name").InnerText;
-            ////    price = node.SelectSingleNode("Product_price").InnerText;
-            ////}
-            //var element1 = from p in xelement.Elements("PointOfSales")
-            //               where (string)p.Element("POS").Attribute("IsActive") == "true" && (string)p.Element("POS").Attribute("RequestType") == "BargainFinder"
-            //               select p;
-            //var e = element1.ElementAt(1).Element("CompanyCode").Value;
-            //var f = element1.ElementAt(1).Element("ID").Value;
-            //var g = element1.ElementAt(1).Element("Type").Value;
-
+            var pos = GetPOS();
+            if (bargainFinder != null && bargainFinder.OTA_AirLowFareSearchRQ!=null && pos != null)
+            bargainFinder.OTA_AirLowFareSearchRQ.POS = pos;
             //TBD : URL configurable using XML
             APIHelper.SetApiKey(_apiCaller, _cacheService);
             String result = _apiCaller.Post("v1.8.2/shop/flights?mode=live", ServiceStackSerializer.Serialize(bargainFinder)).Result;
             var bargainResponse= DeSerializeResponse(result);
             HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, bargainResponse);         
             return response;
+        }
+
+        private POS GetPOS()
+        {
+            XElement xelement = XElement.Load(HostingEnvironment.MapPath("/POS/PointOfSales.xml"));
+
+            IEnumerable<XElement> ps = xelement.Elements("POS")
+                   .Where(x => x.Attribute("IsActive").Value == "true" && x.Attribute("RequestType").Value == "BargainFinder"); 
+            if (ps != null)
+            {
+                POS pos = new POS()
+                {
+                    Source = new List<Source>()
+                };
+                foreach(XElement p in ps)
+                {
+                    pos.Source.Add(new Source()
+                    {
+                        RequestorID = new RequestorID()
+                        {
+                            CompanyName = new CompanyName()
+                            {
+                                Code = p.Element("CompanyCode").Value
+                            },
+                            ID = p.Element("ID").Value,
+                            Type = p.Element("Type").Value
+                        }
+                    });
+                }
+             
+                return pos;
+            }
+            return null;
         }
 
         private LowFareSearch DeSerializeResponse(string result)
