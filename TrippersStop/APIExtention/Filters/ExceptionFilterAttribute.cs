0cf555a92ad311b9eed6e533d94d4edcf0bf738b
Manoj <er.manojkhandelwal@hotmail.com>
2015-04-22 23:19:05 +0530

    Area registration fix

--- TrippersStop/APIExtention/Filters/ExceptionFilterAttribute.cs
+++ TrippersStop/APIExtention/Filters/ExceptionFilterAttribute.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TrippersStop.APIExtention.Filters
+namespace TrippersStop
 {
     using System;
     using System.Net;
