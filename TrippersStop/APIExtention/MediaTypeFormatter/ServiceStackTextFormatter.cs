bda9dbc77969180ae2340a0e46c20eaee6a4ea68
Manoj <er.manojkhandelwal@hotmail.com>
2015-08-30 08:50:04 +0530

    Added new formatter

---
+++ TrippersStop/APIExtention/MediaTypeFormatter/ServiceStackTextFormatter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trippism.APIExtention.MediaTypeFormatter
+{
+    public class ServiceStackTextFormatter : MediaTypeFormatter
+    {
+        public ServiceStackTextFormatter()
+        {
+            JsConfig.DateHandler = JsonDateHandler.ISO8601;
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
+
+            SupportedEncodings.Add(new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true));
+            SupportedEncodings.Add(new UnicodeEncoding(bigEndian: false, byteOrderMark: true, throwOnInvalidBytes: true));
+        }
+
+        public override bool CanReadType(Type type)
+        {
+            if (type == null) throw new ArgumentNullException("type");
+            return true;
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            if (type == null) throw new ArgumentNullException("type");
+            return true;
+        }
+
+        public override Task<object> ReadFromStreamAsync(Type type, Stream readStream, System.Net.Http.HttpContent content, IFormatterLogger formatterLogger)
+        {
+            var task = Task<object>.Factory.StartNew(() => JsonSerializer.DeserializeFromStream(type, readStream));
+            return task;
+        }
+
+        public override Task WriteToStreamAsync(Type type, object value, Stream writeStream, System.Net.Http.HttpContent content, TransportContext transportContext)
+        {
+            var task = Task.Factory.StartNew(() => JsonSerializer.SerializeToStream(value, type, writeStream));
+            return task;
+        }
+    }
+}
