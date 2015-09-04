7c249b081543a6ea0d8956503f73b222789c1555
Jatin Surat <jatindeveloper.surat@gmail.com>
2015-09-04 17:02:20 +0530

    Implementing google  place details api into api project

---
+++ TraveLayer/APIServices/GooglePlaceAPICaller.cs
@@ -0,0 +1,89 @@
+﻿using ServiceStack.Text;
+using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using TraveLayer.CustomTypes.Sabre.Response;
+
+namespace TrippismApi.TraveLayer
+{
+    public class GooglePlaceAPICaller : IAsyncGooglePlaceAPICaller
+    {
+        Uri _BaseAPIUri;
+        public Uri BaseAPIUri
+        {
+            get
+            {
+                return this._BaseAPIUri;
+            }
+            set
+            {
+                this._BaseAPIUri = value;
+            }
+        }
+        String _ClientId;
+
+        public String ClientId
+        {
+            set
+            {
+                this._ClientId = value;
+            }
+        }
+        String _Accept;
+
+        public String Accept
+        {
+            set
+            {
+                this._Accept = value;
+            }
+        }
+        String _ContentType;
+        public String ContentType
+        {
+            set
+            {
+                this._ContentType = value;
+            }
+        }
+
+        public GooglePlaceAPICaller()
+        {
+            _ClientId = ConfigurationManager.AppSettings["GooglePlaceApiKey"].ToString();
+            _BaseAPIUri = new Uri(ConfigurationManager.AppSettings["GooglePlaceDetailApiUri"].ToString() + "?key=" + _ClientId);
+        }
+
+        public async Task<APIResponse> Get(string strPlaceId)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(_Accept));
+
+                HttpResponseMessage googleplaceResponse = await client.GetAsync(this.BaseAPIUri + "&placeid=" + strPlaceId).ConfigureAwait(false);
+
+                if (!googleplaceResponse.IsSuccessStatusCode)
+                {
+                    var ErrorObject = googleplaceResponse.Content.ReadAsStringAsync().Result;
+                    JsonObject error = JsonObject.Parse(ErrorObject);
+                    string errorType = error.Get<string>("error");
+                    string errorDescription = error.Get<string>("error_description");
+                    string message = error.Get<string>("message");
+                    string status = error.Get<string>("status");
+                    string errorMessage = error.Get<string>("message");
+                    string responseMessage = string.Join(" ", errorType, errorDescription, errorMessage).Trim();
+                    return new APIResponse { StatusCode = googleplaceResponse.StatusCode, Response = responseMessage };
+                }
+                var response = await googleplaceResponse.Content.ReadAsStringAsync();
+                return new APIResponse { StatusCode = HttpStatusCode.OK, Response = response };
+            }
+        }
+
+        public Task<APIResponse> Post(string Method, string Body)
+        {
+            return null;
+        }
+    }
+}
---
+++ TraveLayer/CustomTypes/Google/Response/GooglePlaceDetailsOutput.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraveLayer.CustomTypes.Google.Response
+{
+    public class GooglePlaceDetailsOutput
+    {
+        public List<GooglePlaceresults> result { get; set; }
+    }
+
+    public class GooglePlaceresults
+    {
+        public string adr_address { get; set; }
+        public string formatted_address { get; set; }
+        public string formatted_phone_number { get; set; }
+        public GooglePlaceGeometry geometry { get; set; }
+        public string icon { get; set; }
+        public string international_phone_number { get; set; }
+        public string name { get; set; }
+        public List<GooglePlacePhotos> photos { get; set; }
+        public string place_id { get; set; }
+        public string rating { get; set; }
+        public string vicinity { get; set; }
+        public string website { get; set; }
+
+    }
+
+    public class GooglePlaceGeometry
+    {
+        public GooglePlaceLocation location { get; set; }
+    }
+
+    public class GooglePlaceLocation
+    {
+        public string lat { get; set; }
+        public string lng { get; set; }
+    }
+
+    public class GooglePlacePhotos
+    {
+        public string height { get; set; }
+        public string width { get; set; }
+    }
+
+}
