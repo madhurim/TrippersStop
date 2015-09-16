e4368e4fad854e5cbfdf1152c0d8b20e3883e6e1
tgsravi <ravi.p@trivenitechnologies.in>
2015-09-16 15:18:45 +0530

    Created new area for authentication

---
+++ TrippersStop/Areas/Profiles/ProfilesAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Trippism.Areas.Profiles
+{
+    public class ProfilesAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Profiles";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Profiles_default",
+                "Profiles/{controller}/{action}/{id}",
+                  new { controller = "Destinations", action = "Get", id = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
