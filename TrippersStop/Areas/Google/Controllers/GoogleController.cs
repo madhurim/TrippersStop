001aeb68b0ac6bbfe084a3f82f4fa52a55983c63
madhurim <madhuri.mittal@gmail.com>
2016-01-01 15:37:37 -0500

    google attraction related beginning changes

--- TrippersStop/Areas/Google/Controllers/GoogleController.cs
+++ TrippersStop/Areas/Google/Controllers/GoogleController.cs
@@ -9,6 +9,8 @@ using TrippismApi.TraveLayer;
 using AutoMapper;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Collections.Generic;
+
 namespace Trippism.Areas.GooglePlace.Controllers
 {
     public class GooglePlaceController : ApiController
@@ -43,16 +45,68 @@ namespace Trippism.Areas.GooglePlace.Controllers
             _apiCaller.Accept = "application/json";
             _apiCaller.ContentType = "application/json";
 
+           // &types=restaurant|cafe
+            string[] types = locationsearch.Types.Replace("&types=", System.String.Empty).Split('|');
+
             //string strLatitudeandsLongitude = Latitude + "," + Longitude;
             string url = GetURL(locationsearch);
+            //let's just get English language results
+            url = url + "&language=en";
             APIResponse result = _apiCaller.Get(url).Result;
 
             if (result.StatusCode == HttpStatusCode.OK)
             {
                 GoogleOutput googleplace = new GoogleOutput();
                 googleplace = ServiceStackSerializer.DeSerialize<GoogleOutput>(result.Response);
+
                 if (locationsearch.ExcludeTypes != null)
                     googleplace.results = googleplace.results.Where(x => !x.types.Intersect(locationsearch.ExcludeTypes).Any()).ToList();
+                   
+
+                //if next_page_token exists get the next set of results , combine , order by rating get top 20
+                if (!string.IsNullOrWhiteSpace(googleplace.next_page_token))
+                {
+                    result = _apiCaller.Get(url + "&pagetoken=" + googleplace.next_page_token).Result;
+                    GoogleOutput nextResult = ServiceStackSerializer.DeSerialize<GoogleOutput>(result.Response);
+                    if (locationsearch.ExcludeTypes != null)
+                        nextResult.results = nextResult.results.Where(x => !x.types.Intersect(locationsearch.ExcludeTypes).Any()).ToList();
+                    googleplace.results.AddRange(nextResult.results);
+                     
+                    
+                }
+
+                //Filter : the types returned by Google should have the types in the request
+                //Exclude lodging from restaurants
+                //Keyword=Beach : the name in the result should have "beach" , "resort" or "resorts"
+
+                //only if result is acquired with types. if the result is not acquired with types : for ex: Keyword=beach skip this logic
+                if( types.Length > 0 && !types[0].Contains("keyword") )
+                {
+                    var tempcollection = new List<TraveLayer.CustomTypes.Google.Response.results>();
+
+                    foreach (var output in googleplace.results)
+                    {
+                        bool matchfound = false;
+
+                        foreach (string match in types)
+                        {
+                            if (output.types.Contains(match))
+                            { matchfound = true; break; }
+                        }
+                        if (matchfound == false)
+                        { tempcollection.Add(output); }
+
+                    }
+                    foreach (var output in tempcollection)
+                    {
+                        if (googleplace.results.Contains(output)) googleplace.results.Remove(output);
+                    }
+                
+                }
+                
+               //Order by rank : The highest ranked first -
+               googleplace.results = googleplace.results.OrderByDescending(x => x.rating).ToList();
+
 
                 Mapper.CreateMap<GoogleOutput, TraveLayer.CustomTypes.Google.ViewModels.Google>();
                 Mapper.CreateMap<results, TraveLayer.CustomTypes.Google.ViewModels.results>();
@@ -62,6 +116,7 @@ namespace Trippism.Areas.GooglePlace.Controllers
 
                 TraveLayer.CustomTypes.Google.ViewModels.Google lstLocations = Mapper.Map<GoogleOutput, TraveLayer.CustomTypes.Google.ViewModels.Google>(googleplace);
 
+                //is this a bug ? 
                 if (string.IsNullOrWhiteSpace(locationsearch.NextPageToken))
                     _cacheService.Save<TraveLayer.CustomTypes.Google.ViewModels.Google>(cacheKey, lstLocations);
 
