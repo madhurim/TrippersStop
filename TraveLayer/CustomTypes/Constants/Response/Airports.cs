c504a485fd8facda1011be2c9c9ed6ca25d3225b
tgsravi <ravi.p@trivenitechnologies.in>
2016-01-30 16:47:52 +0530

    new airport_downsized.json added with shorten names and MAC rank

--- TraveLayer/CustomTypes/Constants/Response/Airports.cs
+++ TraveLayer/CustomTypes/Constants/Response/Airports.cs
@@ -18,21 +18,39 @@ namespace TraveLayer.CustomTypes.Constants.Response
 
     public class AirportsDetail
     {
-        public string airport_CityCode { get; set; }
-        public string airport_CityName { get; set; }
-        public string airport_CountryCode { get; set; }
-        public string airport_CountryName { get; set; }
+        //public string airport_CityCode { get; set; }
+        //public string airport_CityName { get; set; }
+        //public string airport_CountryCode { get; set; }
+        //public string airport_CountryName { get; set; }
+        //public string region { get; set; }
+        //public bool isMultiAirport { get; set; }
+        //public int? rank { get; set; }
+        //public List<Airport> Airports { get; set; }
+
+        public string cCode { get; set; }   // airport_CityCode
+        public string cName { get; set; }   // airport_CityName
+        public string coCode { get; set; } // airport_CountryCode
+        public string coName { get; set; }  // airport_CountryName
+        public bool isMAC { get; set; } // isMultiAirport
         public string region { get; set; }
-        public bool isMultiAirport { get; set; }
+        public int? rank { get; set; }
         public List<Airport> Airports { get; set; }
     }
     public class Airport
     {
-        public string airport_Code { get; set; }
-        public string airport_FullName { get; set; }
-        public string airport_Lat { get; set; }
-        public string airport_Lng { get; set; }
-        public string alternatenames { get; set; }
-        public string[] themes { get; set; }
+        //public string airport_Code { get; set; }
+        //public string airport_FullName { get; set; }
+        //public string airport_Lat { get; set; }
+        //public string airport_Lng { get; set; }
+        //public string alternatenames { get; set; }
+        //public string[] themes { get; set; }
+        //public int? rank { get; set; }
+        public string code { get; set; }    // airport_Code
+        public string name { get; set; } // airport_FullName
+        public string lat { get; set; } //airport_Lat
+        public string lng { get; set; } //airport_Lng
+        public string names { get; set; } // alternatenames
+        public int? rank { get; set; }
+        public List<string> themes { get; set; }
     }
 }
