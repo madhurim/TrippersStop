31afc2e14bc9f6b8f3026e4c81cbd505cec8b7dc
madhurim <madhuri.mittal@gmail.com>
2016-03-12 21:49:46 -0500

    Insights changes and cleanups

--- TraveLayer/CustomTypes/Sabre/ViewModels/FareRange.cs
+++ TraveLayer/CustomTypes/Sabre/ViewModels/FareRange.cs
@@ -14,6 +14,18 @@ namespace TraveLayer.CustomTypes.Sabre.ViewModels
         public string OriginLocation { get; set; }
         public string DestinationLocation { get; set; }
         public List<FareData> FareData { get; set; }
+        // public List<RecentlyPaid> FareData { get; set; }
+
+    }
+    public class RecentlyPaid
+    {
+        public double MaximumFare { get; set; }
+        public double MinimumFare { get; set; }
+        public double MedianFare { get; set; }
+        public string CurrencyCode { get; set; }
+        public string Count { get; set; }
+        public string DepartureDateTime { get; set; }
+        public string ReturnDateTime { get; set; }
 
     }
 }
