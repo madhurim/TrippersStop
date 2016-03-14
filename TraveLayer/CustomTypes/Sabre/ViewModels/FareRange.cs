c5d8650e8ee00cb279e8e44581a6e26a8fd88e52
madhurim <madhuri.mittal@gmail.com>
2016-03-14 14:55:51 -0400

    Some more minor changes; removed duplications

--- TraveLayer/CustomTypes/Sabre/ViewModels/FareRange.cs
+++ TraveLayer/CustomTypes/Sabre/ViewModels/FareRange.cs
@@ -31,15 +31,5 @@ namespace TraveLayer.CustomTypes.Sabre.ViewModels
        // public List<RecentlyPaid> FareData { get; set; }
 
     }
-    public class RecentlyPaid
-    {
-        public double MaximumFare { get; set; }
-        public double MinimumFare { get; set; }
-        public double MedianFare { get; set; }
-        public string CurrencyCode { get; set; }
-        public string Count { get; set; }
-        public string DepartureDateTime { get; set; }
-        public string ReturnDateTime { get; set; }
-       
-    }
+    
 }
